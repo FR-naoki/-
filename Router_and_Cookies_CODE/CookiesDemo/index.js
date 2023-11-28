a949e76e24139412006a25233998e07964853d22
FR <naoki.maekawa.511@gmail.com>
2023-11-28 14:20:02 +0900

    11/28

--- Router_and_Cookies_CODE/CookiesDemo/index.js
+++ Router_and_Cookies_CODE/CookiesDemo/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser(`mysecret`));
+
+app.get('/greet', (req, res) => {
+    const {name = `anonymous`} = req.cookies;
+    res.send(`ようこそ ${name} さん`);
+});
+
+app.get('/setname', (req, res) => {
+    res.cookie(`name`, `yamadataro`);
+    res.cookie(`animal`, `cat`);
+    res.send(`クッキー送ったよ！！`);
+});
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie(`fruit`, `grape`)
+});
+
 app.listen(3000, () => {
     console.log(`待受中...`);
 });
